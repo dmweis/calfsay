3abae765b1b392cea661772ec647e34db18cc4da
David Weis <davidmichaelweis@gmail.com>
2019-09-03 22:45:52 +0100

    Start implementing wrapping

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,37 @@ use std::fs;
 use std::env;
 use std::io;
 use clap::{Arg, App};
+use textwrap::wrap;
+use std::borrow::Cow;
 
 /// Prints speech bubble for a given text
 /// For now nat handling multiple lines
-fn draw_bubble(text: &str) {
-    let length = text.len() + 2;
+fn draw_bubble(text: &str, wrap_size: usize) {
+
+    let wrapped_lines = wrap(text, wrap_size).iter().map(|line| line.to_mut()).collect::<&mutString>();
+
+    let length = wrapped_lines[0].len() + 2;
     let mut first_line = String::from(" ");
     first_line.push_str(&String::from("_").repeat(length));
     println!("{}", first_line);
 
-    println!("{}{}{}", "< ", text, " >");
+    if wrapped_lines.len() > 1 {
+        println!("{}{}{}", "/ ", wrapped_lines[0], " \\");
+
+        let longest_line = wrapped_lines.iter().max_by(|line| line.len());
+
+        let index_last_line = wrapped_lines.len() - 2;
+        for (index, line) in wrapped_lines.iter().skip(1).enumerate() {
+            if index == index_last_line {
+                println!("{}{}{}", "\\ ", line, " /");
+            } else {
+                println!("{}{}{}", "| ", line, " |");
+            }
+        }
+
+    } else {
+        println!("{}{}{}", "< ", wrapped_lines[0], " >");
+    }
 
     let mut last_line = String::from(" ");
     last_line.push_str(&String::from("-").repeat(length));
@@ -64,12 +85,18 @@ fn main() {
                     .arg(Arg::with_name("cow-file")
                         .short("f")
                         .takes_value(true))
+                    .arg(Arg::with_name("wrap-size")
+                        .short("W")
+                        .takes_value(true))
                     .arg(Arg::with_name("input")
                         .multiple(true))
                     .get_matches();
 
     let desired_file = matches.value_of("cow-file").unwrap_or("default");
     let text = matches.values_of("input").unwrap_or_default().collect::<Vec<_>>().join(" ");
+    let wrap_size = matches.value_of("wrap-size").unwrap_or("40");
+    // Ignoring if input is not a number may not be the best idea
+    let wrap_size = wrap_size.parse::<usize>().unwrap_or(40);
 
     let env_var = env::var("COWPATH")
         .unwrap_or(String::from("cows:/usr/share/cowsay/cows"));
@@ -79,7 +106,7 @@ fn main() {
         let cow_file_content = get_cow_file(desired_file, &path);
 
         if let Ok(contents) = cow_file_content {
-            draw_bubble(&text);
+            draw_bubble(&text, wrap_size);
             draw_cow_file(&contents);
             return;
         }
